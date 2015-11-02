2666ee36413cd70cca019fa67a347c280ddc78a6
Chris Talkington <chris@talkingtontech.com>
2015-11-02 15:57:02 -0600

    create compressed zip on release.

--- config/grunt/aliases.js
+++ config/grunt/aliases.js
@@ -32,6 +32,7 @@ module.exports = function() {
 		'release': [
 			'build',
 			'copy:release'
+			'compress:release'
 		],
 		'default': [
 			'watch'
---
+++ config/grunt/compress.js
@@ -0,0 +1,36 @@
+// https://github.com/gruntjs/grunt-contrib-compress
+module.exports = {
+  release: {
+    options: {
+      archive: '<%= paths.release %><%= pkg.name %>-<%= pkg.version %>.zip'
+    },
+    files: [
+      {
+        expand: true,
+        src: [
+          '**',
+          '.*',
+          '!.git/**',
+          '!.sass-cache/**',
+          '!.jscsrc',
+          '!.jshintrc',
+          '!config/**',
+          '!release/**',
+          '!css/src/**',
+          '!bower_components/**',
+          '!node_modules/**',
+          '!tmp/**',
+          '!*.json',
+          '!*.sublime*',
+          '!.DS_Store',
+          '!.gitattributes',
+          '!.gitignore',
+          '!composer.lock',
+          '!gruntfile.js',
+          '!package.json'
+        ],
+        dest: '<%= pkg.name %>'
+      }
+    ]
+  }
+};
